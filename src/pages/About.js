6a9e89aa2ab1c39aedf4c01fe1cd8525d8b8c3ad
Jinhal01 <jinhalmaheshwari1256@gmail.com>
2023-02-11 15:02:39 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,10 +8,10 @@ import Layout from './../components/Layout/Layout';
 
 const About = () => {
   return (
-    
+  
     <Layout>
       
-            
+          
         <Box sx={{
          
           textAlign:"center",
@@ -35,7 +35,7 @@ const About = () => {
             m:3,
             backgroundColor:'#E4C0B8',
             fontFamily:'Dancing Script' ,
-            textEmphasisColor:'#efebe9',
+            textEmphasisColor:'#000000',
           
           },
           "& p":{
@@ -43,8 +43,8 @@ const About = () => {
              fontSize:'1.5rem',
              backgroundColor:'#E4C0B8',
              padding:2,
-            
-             textEmphasisColor:'#efebe9',
+              textDecorationColor:"#000000",
+             textEmphasisColor:'#000000',
             
           },
           "@media (max-width:1000px)":{
@@ -52,29 +52,31 @@ const About = () => {
             backgroundColor:'#E4C0B8'
           }
         }}>
-         <Typography variant="h2">
+         <Typography variant="h2" sx={{ color: "black" }}>
           Welcome to Dostea!!
           </Typography> 
-          <Typography variant="h4" >
+          <Typography variant="h4" sx={{ color: "white"}}>
             Our Story
           </Typography>
-          <p>
-
+          <p
+          >
+            <Typography variant="h6" sx={{  color: "black" }}>
           We are a surat based startup born on 15 September 2020. <br></br>We 3 friends who are avid tea lovers so we started our very own cafe franchise of Gujarat.<br></br>Our passion for chai, coffee and quick, simple but always delicious meals made us create beautiful memories and amazing experiences from the last 2 years.<br></br> The very first eatable biscuit cup cafe of its type.<br></br>
           *9/10 people like Dostean hot chocolate<br></br>
           Dostea is known for serving chai and hot chocolate in biscuit cups!!<br></br>
-          It’s the perfect place to relax on your own or to catch up with friends while enjoying great coffee and great food.</p>
-          <Typography variant="h2">
+          It’s the perfect place to relax on your own or to catch up with friends while enjoying great coffee and great food.
+          </Typography></p>
+          <Typography variant="h2" sx={{ bgcolor: "black", color: "white" }}>
             Our Food
           </Typography>
-          <p>
+          <p ><Typography variant="h6" sx={{  color: "black" }}>
           Our food is all natural, simple food made with local ingredients, wherever possible, to provide you with fresher, healthier and deliciously tasty meals. We have a well stocked pastry cabinet, our food has an international flavor and we offer gluten free, vegetarian and vegan options
-
+          </Typography>
           </p>
-          <Typography variant="h2">
+          <Typography variant="h2" sx={{  color: "white " }}>
             We Provide
           </Typography>
-          <p>
+          <p><Typography variant="h6" sx={{  color: "black" }}>
             Birthday party DJ<br>
             </br>
             Kitty party<br></br>
@@ -84,7 +86,7 @@ const About = () => {
             Family Party<br></br>
             Anniversary Event<br></br>
             Workshops & many more
-
+            </Typography>
           </p>
 
         </Box>
